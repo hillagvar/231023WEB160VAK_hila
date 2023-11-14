e22190a2227b7d0187668cf84e726313fff291ee
HG <82829500+hillagvar@users.noreply.github.com>
2023-11-14 14:56:45 +0200

    edited

--- Uzduotys/1.1 JS uzduotis/scripts/script.js
+++ Uzduotys/1.1 JS uzduotis/scripts/script.js
@@ -1,7 +1,9 @@
 let svoris = prompt("Įveskite savo svorį kilogramais");
 
 if (svoris < 100) {
-    console.log("Jūs sveriate mažiau negu 100kg");
+  console.log("Jūs sveriate mažiau negu 100kg");
+} else if (svoris > 100) {
+  console.log(`Jūs sveriate daugiau negu 100kg - ${svoris}kg`);
 } else {
-    console.log(`Jūs sveriate daugiau negu 100kg - ${svoris}kg`);
+  console.log("Jūs sveriate 100kg");
 }
