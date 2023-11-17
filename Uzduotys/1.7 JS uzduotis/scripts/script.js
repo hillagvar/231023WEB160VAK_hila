9b8b0679a8f6c8edf0202f588fef9b558572ac3a
HG <82829500+hillagvar@users.noreply.github.com>
2023-11-17 22:54:08 +0200

    su masyvu

--- Uzduotys/1.7 JS uzduotis/scripts/script.js
+++ Uzduotys/1.7 JS uzduotis/scripts/script.js
@@ -1,29 +1,40 @@
-function duokManSavaitesDienaPagalSkaiciu(skaicius) {
-  let savaitesDiena;
+function dienaPagalSkaiciu(skaicius) {
+  let dienos = ["Pirmadienis", "Antradienis", "Trečiadienis", "Ketvirtadienis", "Penktadienis", "Šeštadienis", "Sekmadienis"];
 
-  switch (skaicius) {
-    case 7:
-      savaitesDiena = "Sekmadienis";
-      break;
-    case 6:
-      savaitesDiena = "Šeštadienis";
-      break;
-    case 5:
-      savaitesDiena = "Penktadienis";
-      break;
-    case 4:
-      savaitesDiena = "Ketvirtadienis";
-      break;
-    case 3:
-      savaitesDiena = "Trečiadienis";
-      break;
-    case 2:
-      savaitesDiena = "Antradienis";
-      break;
-    case 1:
-      savaitesDiena = "Pirmadienis";
-  }
-  return savaitesDiena;
+  return dienos[skaicius - 1];
+
 }
 
-console.log(duokManSavaitesDienaPagalSkaiciu(1));
+console.log(dienaPagalSkaiciu(7));
+
+
+
+// function duokManSavaitesDienaPagalSkaiciu(skaicius) {
+//   let savaitesDiena;
+
+//   switch (skaicius) {
+//     case 7:
+//       savaitesDiena = "Sekmadienis";
+//       break;
+//     case 6:
+//       savaitesDiena = "Šeštadienis";
+//       break;
+//     case 5:
+//       savaitesDiena = "Penktadienis";
+//       break;
+//     case 4:
+//       savaitesDiena = "Ketvirtadienis";
+//       break;
+//     case 3:
+//       savaitesDiena = "Trečiadienis";
+//       break;
+//     case 2:
+//       savaitesDiena = "Antradienis";
+//       break;
+//     case 1:
+//       savaitesDiena = "Pirmadienis";
+//   }
+//   return savaitesDiena;
+// }
+
+// console.log(duokManSavaitesDienaPagalSkaiciu(1));
