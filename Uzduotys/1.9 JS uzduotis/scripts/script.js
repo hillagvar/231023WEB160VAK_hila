0e59c907b433c32602052d8ccc46da7d1c6a2b2b
HG <82829500+hillagvar@users.noreply.github.com>
2023-11-18 00:15:05 +0200

    done

---
+++ Uzduotys/1.9 JS uzduotis/scripts/script.js
@@ -0,0 +1,12 @@
+let person = {
+  vardas: "Grzegorz",
+  pavarde: "Brzęczyszczykiewicz",
+  amzius: 52,
+  miestas: "Vilnius",
+  elPastas: "grze5599@belekas.lt",
+  hobiai: ["žvejyba", "grybavimas", "futbolas"]
+};
+
+console.log(person);
+
+// console.log(person.hobiai[0]);
