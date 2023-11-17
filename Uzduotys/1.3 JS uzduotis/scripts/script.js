1a92fa8963d1f5f4f916d0edf017c31b6c505eb4
HG <82829500+hillagvar@users.noreply.github.com>
2023-11-17 23:28:22 +0200

    minor fix

--- Uzduotys/1.3 JS uzduotis/scripts/script.js
+++ Uzduotys/1.3 JS uzduotis/scripts/script.js
@@ -9,25 +9,33 @@ function arPilnametis(gimimoData) {
 
   if (gimimoData.slice(5, 6) === "0") {
     gimimoMenuo = Number(gimimoData.slice(6, 7));
-  } else gimimoMenuo = Number(gimimoData.slice(5, 7));
+  } else {
+    gimimoMenuo = Number(gimimoData.slice(5, 7));
+  }
 
   if (gimimoData.slice(8, 9) === "0") {
     gimimoDiena = Number(gimimoData.slice(9, 10));
-  } else gimimoDiena = Number(gimimoData.slice(8, 10));
+  } else {
+    gimimoDiena = Number(gimimoData.slice(8, 10));
+  }
 
-//   console.log(gimimoMetai, gimimoMenuo, gimimoDiena);
 
   if (dabartiniaiMetai - gimimoMetai < 20) {
-    return false; 
-  } else if (dabartiniaiMetai - gimimoMetai === 20) {
+    return false;
+  }
+  
+  if (dabartiniaiMetai - gimimoMetai === 20) {
     if (dabartinisMenuo < gimimoMenuo) {
       return false;
-    } else if (dabartinisMenuo === gimimoMenuo) {
+    }
+    
+    if (dabartinisMenuo === gimimoMenuo) {
       if (dabartineDiena < gimimoDiena) {
         return false;
       }
     }
   }
+    
   return true;
 }
 
@@ -35,10 +43,6 @@ let gimimoData = prompt("Įveskite savo gimimo datą YYYY-MM-DD");
 
 if (arPilnametis(gimimoData)) {
   console.log("Jūs esate pilnametis");
-  // document.getElementById("exp").innerHTML = "SVEIKINAME, JUMS PARDUOS ALAUS!";
 } else {
   console.log("Jūs esate nepilnametis");
-  // document.getElementById("exp").innerHTML = "DEJA, JUMS NEPARDUOS ALAUS :(";
 }
-
-
