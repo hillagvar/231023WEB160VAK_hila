cb5b90a2ecb6012e635bfaee5ee414632da1385d
HG <82829500+hillagvar@users.noreply.github.com>
2023-11-18 23:17:27 +0200

    done

---
+++ Uzduotys/1.13 JS uzduotis/scripts/script.js
@@ -0,0 +1,12 @@
+const antraste1 = document.getElementById("top-heading");
+
+const antraste2 = document.getElementById("bottom-heading");
+
+const paragrafas1 = document.getElementById("top-paragraph");
+
+const paragrafas2 = document.getElementById("bottom-paragraph");
+
+console.log(antraste1);
+console.log(antraste2);
+console.log(paragrafas1);
+console.log(paragrafas2);
