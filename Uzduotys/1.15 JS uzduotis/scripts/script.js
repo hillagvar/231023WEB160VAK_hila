5438701bc2cdb48fd492cbfedc7b413a7111323d
HG <82829500+hillagvar@users.noreply.github.com>
2023-11-18 23:51:08 +0200

    done

--- Uzduotys/1.15 JS uzduotis/scripts/script.js
+++ Uzduotys/1.15 JS uzduotis/scripts/script.js
@@ -2,8 +2,8 @@ const elementaiPagalKlase = document.querySelectorAll(".coloured");
 
 const elementaiPagalID = document.querySelector("#last-p");
 
-const elemenetaiPagalPavadinima = document.querySelectorAll("p");
+const elementaiPagalPavadinima = document.querySelectorAll("p");
 
 console.log(elementaiPagalKlase);
 console.log(elementaiPagalID);
-console.log(elemenetaiPagalPavadinima);
+console.log(elementaiPagalPavadinima);
