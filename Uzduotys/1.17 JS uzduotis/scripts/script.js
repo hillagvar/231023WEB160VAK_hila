d7139d93eb45f34852e7cc39f34ff40269557157
HG <82829500+hillagvar@users.noreply.github.com>
2023-11-19 01:16:19 +0200

    done

---
+++ Uzduotys/1.17 JS uzduotis/scripts/script.js
@@ -0,0 +1,8 @@
+const nuotrauka = document.querySelector("img");
+
+nuotrauka.setAttribute("src", "https://picsum.photos/300/300");
+nuotrauka.setAttribute("id", "main-photo");
+nuotrauka.setAttribute("class", "photos");
+nuotrauka.setAttribute("name", "first-photo");
+
+
