2df775424f674f6c9c188517eb30670e9cd0f30d
HG <82829500+hillagvar@users.noreply.github.com>
2023-11-19 21:50:27 +0200

    done

--- Uzduotys/1.21 JS uzduotis/scripts/script.js
+++ Uzduotys/1.21 JS uzduotis/scripts/script.js
@@ -3,14 +3,17 @@ document.body.prepend(divas);
 
 let paragrafas = document.createElement("p");
 paragrafas.innerText = "Pusė trijų nakties, labai smagu";
-document.querySelector("div").append(paragrafas);
+divas.append(paragrafas);
 
 let nuotrauka = document.createElement("img");
 nuotrauka.setAttribute("src", "https://picsum.photos/250/250");
-document.querySelector("div").append(nuotrauka);
+divas.append(nuotrauka);
 
 let nuoroda = document.createElement("a");
 nuoroda.innerText = "(Ne)spauskite ant nuorodos";
 nuoroda.setAttribute("href", "https://delfi.lt");
 nuoroda.style.display = "block";
-document.querySelector("div").append(nuoroda);
+divas.append(nuoroda);
+
+
+// document.querySelector("div").append(nuoroda);
---
+++ Uzduotys/1.22 JS uzduotis/scripts/script.js
@@ -0,0 +1,18 @@
+let dezute = document.createElement("div");
+document.body.prepend(dezute);
+
+let para1 = document.createElement("p");
+para1.innerText = "Pirmas paragrafas";
+let para2 = document.createElement("p");
+para2.innerText = "Antras paragrafas";
+let para3 = document.createElement("p");
+para3.innerText = "Trečias paragrafas";
+let para4 = document.createElement("p");
+para4.innerText = "Ketvirtas paragrafas";
+
+dezute.insertAdjacentElement("beforebegin", para1);
+dezute.insertAdjacentElement("afterbegin", para2);
+dezute.insertAdjacentElement("beforeend", para3);
+dezute.insertAdjacentElement("afterend", para4);
+
+
