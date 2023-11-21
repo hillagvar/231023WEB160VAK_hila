d7722a9b2725b1005b6a9f822af895631639a120
HG <82829500+hillagvar@users.noreply.github.com>
2023-11-21 19:15:56 +0200

    alternatives

--- Uzduotys/1.14 JS uzduotis/scripts/script.js
+++ Uzduotys/1.14 JS uzduotis/scripts/script.js
@@ -1,16 +1,33 @@
 const divai = document.getElementsByTagName("div");
 const paragrafai = document.getElementsByTagName("p");
 
-for (let i = 0; i < divai.length; i++) {
-    console.log(divai[i]);
-}
+Array.from(divai).forEach(function(divas) {
+    console.log(divas);
+});
 
-for (let j = 0; j < paragrafai.length; j++) {
-    console.log(paragrafai[j]);
-}
+Array.from(paragrafai).forEach(function(paragrafas) {
+    console.log(paragrafas);
+});
+
+// dar vienas variantas:
+for (let divas of divai) {
+    console.log(divas);
+};
+
+// for (let i = 0; i < divai.length; i++) {
+//     console.log(divai[i]);
+// }
+
+// for (let j = 0; j < paragrafai.length; j++) {
+//     console.log(paragrafai[j]);
+// }
 
 const spalvoti = document.getElementsByClassName("coloured");
 
-for (let x = 0; x < spalvoti.length; x++) {
-    console.log(spalvoti[x]);
-}
+Array.from(spalvoti).forEach(function(spalvotas) {
+    console.log(spalvotas);
+});
+
+// for (let x = 0; x < spalvoti.length; x++) {
+//     console.log(spalvoti[x]);
+// }
