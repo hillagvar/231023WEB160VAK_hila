42b68c0e9c221876b7cd2b339381df3216f123bd
HG <82829500+hillagvar@users.noreply.github.com>
2023-11-21 20:24:52 +0200

    note

--- Uzduotys/1.17 JS uzduotis/scripts/script.js
+++ Uzduotys/1.17 JS uzduotis/scripts/script.js
@@ -3,6 +3,7 @@ const nuotrauka = document.querySelector("img");
 nuotrauka.setAttribute("src", "https://picsum.photos/300/300");
 nuotrauka.setAttribute("id", "main-photo");
 nuotrauka.setAttribute("class", "photos");
+//galima per classList
 nuotrauka.setAttribute("name", "first-photo");
 
 
