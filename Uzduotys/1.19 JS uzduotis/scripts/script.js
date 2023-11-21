ed433fe9cc9edb8214e86726f5c820dcd8bf9bd1
HG <82829500+hillagvar@users.noreply.github.com>
2023-11-21 20:41:58 +0200

    minor fix

--- Uzduotys/1.19 JS uzduotis/scripts/script.js
+++ Uzduotys/1.19 JS uzduotis/scripts/script.js
@@ -1,5 +1,5 @@
 const spanElementai = document.querySelectorAll("p span");
 
-for (i = 0; i < spanElementai.length; i++) {
+for (let i = 0; i < spanElementai.length; i++) {
     spanElementai[i].style.color = "red";
 }
