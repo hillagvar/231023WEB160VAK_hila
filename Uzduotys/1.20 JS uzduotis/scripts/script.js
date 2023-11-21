6c56bfa3325fbdc06c21997203698d8eee40f93c
HG <82829500+hillagvar@users.noreply.github.com>
2023-11-21 21:49:44 +0200

    proper

--- Uzduotys/1.20 JS uzduotis/scripts/script.js
+++ Uzduotys/1.20 JS uzduotis/scripts/script.js
@@ -1,12 +1,22 @@
 let divai = document.getElementsByTagName("div");
+let klases = [];
 
 //removeAttribute("class")
 for (let divas of divai) {
+    klases.push(divas.classList[0]);
     divas.classList.remove(divas.classList[0]);
 }
 
-let klases = ["square","circle","triange","rectangle","pentagon"];
+console.log(klases);
+
+// let klases = ["square","circle","triange","rectangle","pentagon"];
+
+// for (let i = 0; i < divai.length; i++) {
+//         divai[i].classList.add(klases[i]);
+// }
 
 for (let i = 0; i < divai.length; i++) {
-        divai[i].classList.add(klases[i]);
+    let divas = divai[i];
+    let klase = klases[i];
+    divas.classList.add(klase);
 }
